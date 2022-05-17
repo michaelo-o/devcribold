da90dda51b6d5c7f817e7cc3f4bfa40b9cf2f857
m1chae-l <mikeokwuosah@gmail.com>
2022-05-17 02:27:38 +0100

    1

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 const NotFound = () => {
     const router = useRouter()
@@ -10,15 +11,20 @@ const NotFound = () => {
             // router.go(-1)
             // router.go(1)
             router.push('/')
-        }, 3000)
+        }, 5000)
     }, [])
 
     return (
-        <div className="not-found">
-            <h1>Ooops...</h1>
-            <h2>That page cannot be found :(</h2>
-            <p>Going back to the <Link href="/"><a>Homepage</a></Link> is 3 seconds...</p>
-        </div>
+        <>
+            <Head>
+                <title>Not Found in the Crib</title>
+            </Head>
+            <div className="not-found">
+                <h1>Ooops...</h1>
+                <h2>Looks like this page doesn't exist in the crib :(</h2>
+                <p>Going back to the <Link href="/"><a>Homepage</a></Link> in 5 seconds...</p>
+            </div>
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,10 +11,36 @@ export default function Home() {
         <meta name="description" content="Dev Crib" />
       </Head>
       <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-
-
+        <div className="learntocode">
+          <div className="innerlearntocode">
+            <h1>Learn to Code</h1>
+            <h3>Kick start your Front-End Web Developer Career</h3>
+            <Link href="#">
+              <a> <h4>Don't Know Where to Begin?</h4></a>
+            </Link>
+          </div>
+        </div>
+        <div className="gradient1">
+          <p> </p>
+        </div>
+        <div className="learnhtml">
+          <div className="innerlearnhtml">
+            <h1>Learn HTML</h1>
+            <h3>The building blocks that make up the bodies of all Web Pages</h3>
+            <h4>Learn HTML</h4>
+            <h4>HTML References</h4>
+          </div>
+          <div className="example">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nam autem, non deserunt iure harum tempore natus veniam quos pariatur? Unde minima officia officiis sapiente expedita optio quasi incidunt corrupti.
+            
+          </div>
+        </div>
+        <div className="learncss">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum labore voluptates dolorem fuga, illum adipisci est! Quasi deleniti qui ipsam inventore, animi recusandae eum quia, iusto expedita ratione, reiciendis ab.
+        </div>
+        <div className="learnjavascript">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda eveniet vitae amet, nulla dolore similique quod. Temporibus quaerat laborum nemo adipisci ipsa! Pariatur in corporis quas nihil debitis voluptates dolores!
+        </div>
 
 
 
