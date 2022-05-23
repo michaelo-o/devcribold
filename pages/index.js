63ef3535f55b3e5f0ad9806ba7ec45664b848b44
m1chae-l <mikeokwuosah@gmail.com>
2022-05-23 14:57:50 +0100

    workijndl

--- pages/index.js
+++ pages/index.js
@@ -62,6 +62,8 @@ export default function Home() {
                   <br />
                   <br />
                   {BrO}h1{BrC}<span>This is a heading</span>{BrO}/h1{BrC} <br />
+                  {BrO}h2{BrC}<span>This is a Sub-heading</span>{BrO}/h2{BrC} <br />
+                  <br />
                   {BrO}p{BrC}<span>This is a paragraph.</span>{BrO}/p{BrC}
                   <br />
                   <br />
@@ -81,14 +83,20 @@ export default function Home() {
               <h2>CSS Example:</h2>
               <div className="innerllcexample">
                 <p className="inlinecss">
-                  <span className="cssselector">body</span> {CBrO}  <br />
-                  <span className="cssselector">background-color:</span> lightblue;  <br />
+                  body {CBrO}  <br />
+                  <span className="cselector">background-color:</span> <span className="cssvalue">lightblue</span>;  <br />
                   {CBrC}
                   <br />
                   <br />
                   h1 {CBrO}  <br />
-                  <span className="cssselector">color:</span> white;  <br />
-                  <span className="cssselector">text-align:</span> center;  <br />
+                  <span className="cselector">color:</span> <span className="cssvalue">white</span>;  <br />
+                  <span className="cselector">text-align:</span> <span className="cssvalue">center</span>;  <br />
+                  {CBrC}
+                  <br />
+                  <br />
+                  .sampleclass {CBrO} <br />
+                  <span className="cselector">display:</span> <span className="cssvalue">flex</span>; <br />
+                  <span className="cselector">flex-direction</span> <span className="cssvalue">row</span>; <br />
                   {CBrC}
                 </p>
               </div>
@@ -102,28 +110,45 @@ export default function Home() {
             <h4>CSS References</h4>
           </div>
         </div>
+        <div className="gradient3">
+          <p> </p>
+        </div>
         <div className="learnjavascript">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda eveniet vitae amet, nulla dolore similique quod. Temporibus quaerat laborum nemo adipisci ipsa! Pariatur in corporis quas nihil debitis voluptates dolores!
+          <div className="leftlearnjs">
+            <h1>Learn</h1>
+            <h1>JavaScript</h1>
+            <h3>The scripting language used for programming Web Pages</h3>
+            <h4>Learn JavaScript</h4>
+            <h4>JavaScript References</h4>
+          </div>
+          <div className="rightlearnjs">
+            <div className="innerrlj">
+              <h2>JavaScript Example: </h2>
+              <div className="innerrljexample">
+                <p className="inlinejs">
+                  {BrO}button <span className="jsvh">onclick</span>="myFunction()"{BrC}<span>Click Me!</span>{BrO}/button{BrC}
+                  <br />
+                  <br />
+                  {BrO}script{BrC} <br />
+                  <span className="jsvh">function</span> myFunction() {CBrO} <br />
+                  <span className="tabbedjs"><span className="jsvh">let</span> x = document.getElementById(<span className="jsfunval">"demo"</span>); <br /></span>
+                  <span className="tabbedjs">x.style.fontSize = <span className="jsfunval">"25px"</span>; <br /></span>
+                  <span className="tabbedjs"> x.style.color = <span className="jsfunval">"red"</span>; <br />
+                  </span>
+                  {CBrC} <br />
+                  <br />
+                  <span className="jsvh">const</span><span> d =</span> <span className="jsvh">new</span> <span>Date();</span> <br />
+                  <span><span className="jsvh">let</span> x = document.getElementById(<span className="jsfunval">"datesample"</span>).innerHTML = d;</span> <br />
+                  {BrO}/script{BrC}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <div className="gradient4">
+          <p> </p>
+        </div>
       </div>
 
 
