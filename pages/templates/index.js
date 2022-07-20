f12cef70866ecc0c0dd6e9ed2a0d1bdc94d7721c
m1chae-l <mikeokwuosah@gmail.com>
2022-07-20 11:09:41 +0100

    qq

--- pages/templates/index.js
+++ pages/templates/index.js
@@ -3,13 +3,23 @@ import Head from "next/head";
 import { useRouter } from 'next/router';
 import styles from "../exercises/exercisesgen.module.css"
 import PageContentFooter from "../../components/PageContentFooter";
-import { useAuthContext } from "../../context/AuthContext"
-
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 
 
 const Templates = () => {
 
+
+    const router = useRouter()
+
+    const { user } = useAuthContext()
+
+    const redirect = () => {
+        setTimeout(() => {
+            router.push('/login/')
+        }, 3000)
+    }
+    // !user && redirect()
 
 
 
@@ -23,41 +33,42 @@ const Templates = () => {
 
             <div className={styles.genexcbackground}>
 
-
-                <div className={styles.exhomepagecontent}>
-                    <h1>Responsive Templates</h1>
-
-
-                    <h2>More Templates coming soon</h2>
+                {user &&
+                    <div className={styles.exhomepagecontent}>
+                        <h1>Responsive Templates</h1>
 
 
-                    <div className={styles.extraborder}>
+                        <h2>More Templates coming soon</h2>
 
-                        <div className="embedDiv">
-                            <iframe height="500" style={{ "width": "90%" }}
-                                scrolling="no" title="Responsive Template 1" src="https://codepen.io/_michaeli/embed/KKomPpE?default-tab=html%2Cresult&editable=true&theme-id=dark" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen={true}>
-                                See the Pen <Link href="https://codepen.io/_michaeli/pen/KKomPpE"><a>
-                                    Responsive Template 1</a></Link> by Michael Okwuosah (<Link href="https://codepen.io/_michaeli"><a>@_michaeli</a></Link>)
-                                on <Link href="https://codepen.io"><a>CodePen.</a></Link>
-                            </iframe>
+
+                        <div className={styles.extraborder}>
+
+                            <div className="embedDiv">
+                                <iframe height="500" style={{ "width": "90%" }}
+                                    scrolling="no" title="Responsive Template 1" src="https://codepen.io/_michaeli/embed/KKomPpE?default-tab=html%2Cresult&editable=true&theme-id=dark" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen={true}>
+                                    See the Pen <Link href="https://codepen.io/_michaeli/pen/KKomPpE"><a>
+                                        Responsive Template 1</a></Link> by Michael Okwuosah (<Link href="https://codepen.io/_michaeli"><a>@_michaeli</a></Link>)
+                                    on <Link href="https://codepen.io"><a>CodePen.</a></Link>
+                                </iframe>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.extraborder}>
+                        <div className={styles.extraborder}>
 
-                        <div className="embedDiv">
-                            <iframe height="500" style={{ "width": "90%" }}
-                                scrolling="no" title="Responsive Template 2" src="https://codepen.io/_michaeli/embed/yLKbBXB?default-tab=html%2Cresult&editable=true&theme-id=dark" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen={true}>
-                                See the Pen <Link href="https://codepen.io/_michaeli/pen/yLKbBXB"><a>
-                                    Responsive Template 2</a></Link> by Michael Okwuosah (<Link href="https://codepen.io/_michaeli"><a>@_michaeli</a></Link>)
-                                on <Link href="https://codepen.io"><a>CodePen.</a></Link>
-                            </iframe>
+                            <div className="embedDiv">
+                                <iframe height="500" style={{ "width": "90%" }}
+                                    scrolling="no" title="Responsive Template 2" src="https://codepen.io/_michaeli/embed/yLKbBXB?default-tab=html%2Cresult&editable=true&theme-id=dark" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen={true}>
+                                    See the Pen <Link href="https://codepen.io/_michaeli/pen/yLKbBXB"><a>
+                                        Responsive Template 2</a></Link> by Michael Okwuosah (<Link href="https://codepen.io/_michaeli"><a>@_michaeli</a></Link>)
+                                    on <Link href="https://codepen.io"><a>CodePen.</a></Link>
+                                </iframe>
+                            </div>
                         </div>
+
+
+                        <PageContentFooter />
                     </div>
-
-
-                    <PageContentFooter />
-                </div>
+                }
             </div>
         </>
     );
